f4dec0198cb5f536d209796007947bd7b2510562
Siôn le Roux <sinisterstuf@gmail.com>
2022-11-04 00:16:45 +0100

    Fix comments in the zombie file
    These accidentally still mentioned the "player" because they were
    copy-pasted from there.

--- zombie.go
+++ zombie.go
@@ -8,32 +8,33 @@ import (
 	"github.com/solarlune/resolv"
 )
 
-// Zombie is the player character in the game
+// Zombie is a monster that's trying to eat the player character
 type Zombie struct {
 	Object *resolv.Object
 	Angle  float64
 }
 
-// MoveUp moves the player upwards
+// MoveUp moves the zombie upwards
 func (z *Zombie) MoveUp() {
 	z.move(0, -1)
 }
 
-// MoveDown moves the player downwards
+// MoveDown moves the zombie downwards
 func (z *Zombie) MoveDown() {
 	z.move(0, 1)
 }
 
-// MoveLeft moves the player left
+// MoveLeft moves the zombie left
 func (z *Zombie) MoveLeft() {
 	z.move(-1, 0)
 }
 
-// MoveRight moves the player right
+// MoveRight moves the zombie right
 func (z *Zombie) MoveRight() {
 	z.move(1, 0)
 }
 
+// Move the Zombie by the given vector if it is possible to do so
 func (z *Zombie) move(dx, dy float64) {
 	if collision := z.Object.Check(dx, dy, "mob", "wall"); collision == nil {
 		z.Object.X += dx
