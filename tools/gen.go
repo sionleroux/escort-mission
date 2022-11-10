5f296e6bab9e68dadb1e9049ad87023ab4ce2a33
Siôn le Roux <sinisterstuf@gmail.com>
2022-11-10 16:28:15 +0100

    WIP: example from https://pkg.go.dev/text/template

---
+++ tools/gen.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"os"
+	"text/template"
+)
+
+type Inventory struct {
+	Material string
+	Count    uint
+}
+
+func main() {
+	sweaters := Inventory{"wool", 17}
+	tmpl, err := template.New("test").Parse("{{.Count}} items are made of {{.Material}}")
+	if err != nil {
+		panic(err)
+	}
+	err = tmpl.Execute(os.Stdout, sweaters)
+	if err != nil {
+		panic(err)
+	}
+}
