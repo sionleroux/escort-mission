c0765dbffb952f1780601ce0fc8cc98e216499a5
Siôn le Roux <sinisterstuf@gmail.com>
2022-11-28 14:28:56 +0100

    Add cool fade in/out animation for start screen text
    Also a drop shadow... there's a weird flicker near full opacity but I'm
    not sure why.

--- startscreen.go
+++ startscreen.go
@@ -7,20 +7,28 @@ import (
 	"image/color"
 
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/tanema/gween"
+	"github.com/tanema/gween/ease"
 	"github.com/tinne26/etxt"
 )
+
+const startText = "Press space to start"
 
 // StartScreen is the first screen you see when you start the game, it shows you
 // a menu that lets you start a game or change game options etc.
 type StartScreen struct {
 	background   *ebiten.Image
 	textRenderer *StartTextRenderer
+	textFader    *gween.Sequence
 }
 
 func NewStartScreen(game *Game) *StartScreen {
+	fadeSeq := gween.NewSequence(gween.New(50, 200, 60, ease.OutQuad))
+	fadeSeq.SetYoyo(true)
 	return &StartScreen{
 		background:   loadImage("assets/splash-screen.png"),
 		textRenderer: NewStartTextRenderer(),
+		textFader:    fadeSeq,
 	}
 }
 
@@ -31,18 +39,22 @@ func (s *StartScreen) Update() (GameState, error) {
 		return gameIntro, nil
 	}
 
+	alpha, _, _ := s.textFader.Update(1)
+	s.textRenderer.alpha = uint8(alpha)
+
 	return gameStart, nil
 }
 
 // Draw renders the start screen to the screen
 func (s *StartScreen) Draw(screen *ebiten.Image) {
 	screen.DrawImage(s.background, &ebiten.DrawImageOptions{})
-	s.textRenderer.Draw(screen, "Press space to start")
+	s.textRenderer.Draw(screen, startText)
 }
 
 // StartTextRenderer wraps etxt.Renderer to draw full-screen text
 type StartTextRenderer struct {
 	*etxt.Renderer
+	alpha uint8
 }
 
 // NewStartTextRenderer creates a text renderer for text on the start screen
@@ -50,13 +62,15 @@ func NewStartTextRenderer() *StartTextRenderer {
 	font := loadFont("assets/fonts/PixelOperator8-Bold.ttf")
 	r := etxt.NewStdRenderer()
 	r.SetFont(font)
-	r.SetColor(color.RGBA{0xff, 0xff, 0xff, 0xff})
 	r.SetAlign(etxt.YCenter, etxt.XCenter)
 	r.SetSizePx(8)
-	return &StartTextRenderer{r}
+	return &StartTextRenderer{r, 0}
 }
 
 func (r StartTextRenderer) Draw(screen *ebiten.Image, text string) {
 	r.SetTarget(screen)
+	r.SetColor(color.RGBA{0x0, 0x0, 0x0, r.alpha})
+	r.Renderer.Draw(text, screen.Bounds().Dx()/2+1, screen.Bounds().Dy()/8*7+1)
+	r.SetColor(color.RGBA{0xff, 0xff, 0xff, r.alpha})
 	r.Renderer.Draw(text, screen.Bounds().Dx()/2, screen.Bounds().Dy()/8*7)
 }
